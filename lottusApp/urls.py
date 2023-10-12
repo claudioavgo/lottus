88c6dd7fa3e1ccae78f5b845be87ec4b3fcdbd35
Cláudio <claudioalvesoficial@gmail.com>
2023-10-12 15:16:33 -0300

    +HOME, +DASHBOARD TEMPLATE

--- lottusApp/urls.py
+++ lottusApp/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.home, name="home"),
+    path("dashboard", views.dashboard, name="dashboard"),
+    path("login", views.login, name="login"),
+    path("cadastro", views.register, name="cadastro"),
 ]
--- lottusApp/views.py
+++ lottusApp/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Página principal
 def home(request):
     return render(request, "home/home.html")
+
+def dashboard(request):
+    return render(request, "dashboard/dash-home.html")
+
+def login(request):
+    return render(request, "auth/login.html")
+
+def register(request):
+    return render(request, "auth/register.html")
