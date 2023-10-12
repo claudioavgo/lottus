918c6fb25873788396c3c820b293bcacfe6f258a
lauravidall <lbv2@cesar.school>
2023-10-12 17:48:48 -0300

    oioi

--- lottusApp/models.py
+++ lottusApp/models.py
@@ -1,8 +1,9 @@
-from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.db import models
 
 # Create your models here.
+
 
 class Children(models.Model):
     nome = models.CharField(max_length=256)
@@ -13,3 +14,14 @@ class Children(models.Model):
 
     def __str__(self):
         return self.nome
+
+
+class Perfil(models.Model):
+    cpf = models.CharField(max_length=14)
+    telefone = models.IntegerField()
+    crianca = models.OneToOneField(
+        blank=True, on_delete=models.CASCADE, to=Children)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.usuario.username
