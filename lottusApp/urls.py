b74e339746c5078aa736f4e781388014afc6fa2b
Cláudio <claudioalvesoficial@gmail.com>
2023-10-13 20:20:22 -0300

    🔧 Correção do login/register

--- lottusApp/urls.py
+++ lottusApp/urls.py
@@ -4,10 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("dashboard", views.dashboard, name="dashboard"),
     path("login", views.loginPage, name="login"),
     path("logout", views.logoutPage, name="logout"),
     path("cadastro", views.register, name="cadastro"),
     path("doar", views.doar, name="doar"),
     path("apadrinhe", views.apadrinhe, name="apadrinhe"),
+    path("dashboard", views.dashboard, name="dashboard"),
+    path("dashboard/contrato", views.contrato, name="contrato"),
 ]
