d95ef23796baa81a587f1e8fbae8f4dc6b7bf0ea
Cláudio <claudioalvesoficial@gmail.com>
2023-10-15 16:37:24 -0300

    🔧 Some fixes

--- lottus/settings.py
+++ lottus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n!16@)nu&74&0*(xqkvwcn8q@j2_^$+10n9ckuo#zu_3z!hb=!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','cultiveapp.azurewebsites.net']
 
 
 # Application definition
