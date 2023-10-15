409f8d8f0fc99d195b0bda7d507c55151bf5bcfc
Guilherme Cardozo <guigao.cardozo@gmail.com>
2023-10-15 17:09:27 -0300

    dashboard página principal + minhas doações

--- lottus/settings.py
+++ lottus/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n!16@)nu&74&0*(xqkvwcn8q@j2_^$+10n9ckuo#zu_3z!hb=!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','cultive.azurewebsites.net']
 
