678e5e4712633b51bcc261326a567ae7e61f617e
Cláudio Alves <64589027+claudioavgo@users.noreply.github.com>
2023-11-30 04:47:47 -0300

    Update settings.py

--- lottus/settings.py
+++ lottus/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-n!16@)nu&74&0*(xqkvwcn8q@j2_^$+10n9ckuo#zu_3z!hb=!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','lottuscultive.azurewebsites.net', 'https://lottus.azurewebsites.net/']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','lottuscultive.azurewebsites.net', 'lottus.azurewebsites.net/']
 CSRF_TRUSTED_ORIGINS = ['https://*.azurewebsites.net','https://*.127.0.0.1', 'http://127.0.0.1']
 
 
